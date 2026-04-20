56036d14f5ebd5cc361e46369689d0653ea556f2
Jonas <jmsh23@student.aau.dk>
2026-04-20 15:08:01 +0200

    Rover entrypoint update

--- ros2_ws/src/mapping_launch/launch/rover_start.launch.py
+++ ros2_ws/src/mapping_launch/launch/rover_start.launch.py
@@ -18,7 +18,7 @@ def generate_launch_description():
 
     nav2_pkg = get_package_share_directory('nav2_bringup')
     nav2_launch = os.path.join(nav2_pkg, 'launch', 'navigation_launch.py')
-    nav2_params = os.path.expanduser('~/ros2_ws/src/nav2_config/config/nav2_params_sim.yaml')
+    nav2_params = os.path.expanduser('~/ros2_ws/src/navigation2/nav2_bringup/params/nav2_params.yaml')
 
 
     return LaunchDescription([
@@ -76,6 +76,10 @@ def generate_launch_description():
 
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(nav2_launch),  
+            launch_arguments={
+                'params_file': nav2_params,
+                'use_sim_time': 'true'
+            }.items()   
         ),
     ])
 """     
