9c380fd7fe6bf619899faeeb231f08b2bc544c92
Jonas <jmsh23@student.aau.dk>
2026-04-23 11:05:25 +0200

    Livox start added to entrypoint

--- docker/rover/entrypoint.sh
+++ docker/rover/entrypoint.sh
@@ -9,6 +9,7 @@ colcon build --packages-select livox_ros_driver2 fast_lio calibrate_lidars mappi
 colcon build --symlink-install --packages-up-to multi_lidar_calibrator --cmake-args -DCMAKE_BUILD_TYPE=Release
 colcon build --base-paths src/navigation2 --packages-ignore nav2_system_tests
 
+ros2 launch livox_ros_driver2 msg_MID360_rover_launch.py &
 ros2 launch mapping_launch system.launch.py rviz:=true start_drone:=false &
 ros2 launch nav2_bringup navigation_launch.py use_sim_time:=false params_file:="/root/ros2_ws/src/navigation2/nav2_bringup/params/nav2_params.yaml" &
 
