a763ddf453ca52fcc6d327879de55bc3f5fcfd9f
Jonas <jmsh23@student.aau.dk>
2026-04-24 10:57:57 +0200

    Remover discovery server from entrypoint

--- docker/rover/entrypoint.sh
+++ docker/rover/entrypoint.sh
@@ -5,13 +5,11 @@ set -e
 source /opt/ros/humble/setup.bash
 source /root/ros2_ws/install/setup.bash
 
-export ROS_DISCOVERY_SERVER=192.168.0.200:11811
+unset ROS_DISCOVERY_SERVER
 
 colcon build --packages-select livox_ros_driver2 fast_lio calibrate_lidars mapping_launch livox_converter leo leo_msgs leo_teleop leo_description leo_gz_bringup leo_gz_plugins leo_gz_worlds leo_simulator leo_simulator simulator_bringup mapping_launch nav2_config odom_to_tf_ros2 merge_map drone_controller
 colcon build --symlink-install --packages-up-to multi_lidar_calibrator --cmake-args -DCMAKE_BUILD_TYPE=Release
 colcon build --base-paths src/navigation2 --packages-ignore nav2_system_tests
-
-fastdds discovery --server-id 0 -l 192.168.0.200&
 
 ros2 launch livox_ros_driver2 msg_MID360_rover_launch.py &
 ros2 launch mapping_launch system.launch.py rviz:=false start_drone:=false &
