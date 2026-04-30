a3e137c3cf937deb25fdb084b08328bc23541a60
Tobiaslr <trauh23@student.aau.dk>
2026-04-30 12:41:00 +0200

    -||-

--- ros2_ws/src/drone_controller/launch/drone_controller_launch.py
+++ ros2_ws/src/drone_controller/launch/drone_controller_launch.py
@@ -36,7 +36,7 @@ def generate_launch_description():
             output='screen',
         ),
         TimerAction(
-            period=15.0,
+            period=10.0,
             actions=[
                 Node(
                     package='drone_controller',
@@ -52,6 +52,6 @@ def generate_launch_description():
                 ),
             ],
         ),
-        TimerAction(period=3.0, actions=[livox_launch]),
-        TimerAction(period=5.0, actions=[mapping_launch]),
+        TimerAction(period=15.0, actions=[livox_launch]),
+        TimerAction(period=20.0, actions=[mapping_launch]),
     ])
